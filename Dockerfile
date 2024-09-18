e26a9052779687eedf73f0515cbc541ef663ae54
xiemulei <xiemulei@gmail.com>
2024-09-19 00:29:24 +0800

    deploy to DigitalOcean

---
+++ Dockerfile
@@ -0,0 +1,27 @@
+FROM rust:1.81.0 AS builder
+# 把工作目录切换到 `app`
+WORKDIR /app
+# 安装所需系统依赖
+RUN sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources
+RUN apt-get update && apt-get install lld clang -y
+# 将工作环境中的所有文件复制到 Docker 镜像中
+COPY . .
+COPY ./debian/config.toml  /usr/local/cargo
+ENV SQLX_OFFLINE=true
+# 开始构建二进制文件
+RUN cargo build --release
+
+# 运行时阶段
+FROM debian:bookworm-slim AS runtime
+WORKDIR /app
+RUN sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources
+RUN apt-get update \
+    && apt-get install -y --no-install-recommends openssl ca-certificates \
+    && apt-get autoremove -y \
+    && apt-get clean -y \
+    && rm -rf /var/lib/apt/lists/*
+COPY --from=builder /app/target/release/zero2prod zero2prod
+COPY configuration configuration
+ENV APP_ENVIRONMENT=production
+# 当执行 `docker run` 时，启动二进制文件
+ENTRYPOINT ["./zero2prod"]
--- src/configuration.rs
+++ src/configuration.rs
@@ -3,7 +3,7 @@ use secrecy::{ExposeSecret, Secret};
 #[derive(serde::Deserialize)]
 pub struct Settings {
     pub database: DatabaseSettings,
-    pub application_port: u16,
+    pub application: ApplicationSettings,
 }
 
 #[derive(serde::Deserialize)]
@@ -15,13 +15,32 @@ pub struct DatabaseSettings {
     pub database_name: String,
 }
 
+#[derive(serde::Deserialize)]
+pub struct ApplicationSettings {
+    pub port: u16,
+    pub host: String,
+}
+
+pub enum Environment {
+    Local,
+    Production,
+}
+
 pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    let base_path = std::env::current_dir().expect("Failed to determine the current directory");
+    let configuration_directory = base_path.join("configuration");
+
+    // 检查运行时环境
+    // 如果没有指定，则默认是 `local`
+    let environment: Environment = std::env::var("APP_ENVIRONMENT")
+        .unwrap_or_else(|_| "local".into())
+        .try_into()
+        .expect("Failed to parse APP_ENVIRONMENT.");
+    let environment_filename = format!("{}.yaml", environment.as_str());
     // 初始化配置读取器
     let settings = config::Config::builder()
-        .add_source(config::File::new(
-            "configuration.yaml",
-            config::FileFormat::Yaml,
-        ))
+        .add_source(config::File::from(configuration_directory.join("base.yaml")))
+        .add_source(config::File::from(configuration_directory.join(&environment_filename)))
         .build()?;
 
     settings.try_deserialize::<Settings>()
@@ -42,3 +61,29 @@ impl DatabaseSettings {
         ))
     }
 }
+
+impl Environment {
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            Environment::Local => "local",
+            Environment::Production => "production",
+        }
+    }
+}
+
+impl TryFrom<String> for Environment {
+    type Error = String;
+
+    fn try_from(s: String) -> Result<Self, Self::Error> {
+        match s.to_lowercase().trim() {
+            "local" => Ok(Self::Local),
+            "production" => Ok(Self::Production),
+            other => Err(format!(
+                "{} is not a supported environment. Use either `local` or `production`.",
+                other
+            )),
+        }
+    }
+}
+
+
